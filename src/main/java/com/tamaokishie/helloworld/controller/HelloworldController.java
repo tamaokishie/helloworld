f1e18cdf83a7df128897bb5d120b2624ed0187cc
tamaokishie <shie0041@gmail.com>
2025-02-06 14:25:39 +0900

    add cors設定

--- src/main/java/com/tamaokishie/helloworld/controller/HelloworldController.java
+++ src/main/java/com/tamaokishie/helloworld/controller/HelloworldController.java
@@ -1,9 +1,11 @@
 package com.tamaokishie.helloworld.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class HelloworldController {
 
     @GetMapping("/hello")
